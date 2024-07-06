eb734e48c9fb27b9596632cedd6aaada6d105f98
Diego Rios <73723371+dandresrp@users.noreply.github.com>
2024-07-05 21:22:29 -0600

    Add backgrounds and changed .bashrc,wezterm configs

--- debian/.config/wezterm/wezterm.lua
+++ debian/.config/wezterm/wezterm.lua
@@ -1,10 +1,15 @@
 local wezterm = require 'wezterm'
-
 local config = wezterm.config_builder()
+local mux = wezterm.mux
 
-config.color_scheme = 'Catppuccin Mocha'
-config.font = wezterm.font 'FiraCode Nerd Font'
+config.color_scheme = 'Dark+'
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
 config.font_size = 16.0
 config.enable_tab_bar = false
+
+wezterm.on("gui-startup", function()
+    local tab, pane, window = mux.spawn_window {}
+    window:gui_window():maximize()
+end)
 
 return config
